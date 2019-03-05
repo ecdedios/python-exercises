347d8fe7ea0be7fd090a822924fd1e25373d91f5
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-03-05 12:13:48 -0600

    tweak code

--- 4.3_control_structures_exercises.py
+++ 4.3_control_structures_exercises.py
@@ -15,8 +15,8 @@ print(line)
 # 1b. prompt the user for a day of the week, print out whether the day is a
 #    weekday or a weekend
 
-weekdays = ['monday','tuesday','wednesday','thursday','friday']
-weekend = ['saturday','sunday']
+weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+weekend = ['saturday', 'sunday']
 
 day_of_the_week = input('Day of the week? ')
 
@@ -44,9 +44,11 @@ hourly_rate = input('How much is the hourly rate?')
 if hours_worked_in_week.isnumeric():
     if hourly_rate.isnumeric():
         if float(hours_worked_in_week) > 40:
-            paycheck_for_week = float(hours_worked_in_week) * (float(hourly_rate) * 1.5)
+            paycheck_for_week = float(
+                hours_worked_in_week) * (float(hourly_rate) * 1.5)
         elif float(hours_worked_in_week) <= 40:
-            paycheck_for_week = float(hours_worked_in_week) * (float(hourly_rate) * 1.0)
+            paycheck_for_week = float(
+                hours_worked_in_week) * (float(hourly_rate) * 1.0)
         else:
             paycheck_for_week = 'ERROR'
     else:
@@ -59,227 +61,186 @@ else:
 if paycheck_for_week == 'ERROR':
     print(f'There has been an error.')
 else:
-  print(f'{paycheck_for_week:,} is the weeks paycheck.')
+    print(f'{paycheck_for_week:,} is the weeks paycheck.')
 
 print(line)
-
 
 
 # 2a - WHILE LOOPS
 
 i = 5
 while i <= 15:
-	print(i)
-	i += 1
+    print(i)
+    i += 1
 
 i = 0
 while i <= 100:
-	print(i)
-	i += 2
+    print(i)
+    i += 2
 
 i = 100
 while i >= -10:
-	print(i)
-	i -= 5
+    print(i)
+    i -= 5
 
 i = 2
 while i < 1_000_000:
-	print(i)
-	i *= i
+    print(i)
+    i *= i
 
 i = 100
 while i >= 5:
-	print(i)
-	i -= 5
+    print(i)
+    i -= 5
 
 # 2b - FOR LOOPS
 
 number = input('Enter a number: ')
 for i in range(11):
-	print(f'{number} x {i} = ' + str((float(number)*i)))
+    print(f'{number} x {i} = ' + str((float(number)*i)))
 
 for i in range(10):
-	print(str(i)*i)
+    print(str(i)*i)
 
 
-
-#2c - BREAK and CONTINUE
+# 2c - BREAK and CONTINUE
 
 number = input('Enter a number between 1 and 50: ')
 while not number.isdigit():
-	number = input('Enter a number between 1 and 50: ')
+    number = input('Enter a number between 1 and 50: ')
 while not (int(number) > 0 and int(number) < 51):
-	number = input('Enter a number between 1 and 50: ')
+    number = input('Enter a number between 1 and 50: ')
 while (int(number) % 2) == 0:
-	number = input('Enter a number between 1 and 50: ')
-print('The number to skip is: '+ number)
+    number = input('Enter a number between 1 and 50: ')
+print('The number to skip is: ' + number)
 print()
-for i in range(1,51,2):
-	if i == int(number):
-		print('Yikes! Skipping number: ' + str(i))
-		continue
-	print('Here is an odd number: ' + str(i))
+for i in range(1, 51, 2):
+    if i == int(number):
+        print('Yikes! Skipping number: ' + str(i))
+        continue
+    print('Here is an odd number: ' + str(i))
 
 number = input('Enter a positive number: ')
 while not number.isdigit():
-	number = input('Not a positive number. Enter a positive number:  ')
+    number = input('Not a positive number. Enter a positive number:  ')
 while not (int(number) > 0):
-	number = input('Not a positive number. Enter a positive number:  ')
-for i in range(0,int(number)+1):
-	print(i)
+    number = input('Not a positive number. Enter a positive number:  ')
+for i in range(0, int(number)+1):
+    print(i)
 
 number = input('Enter a positive number: ')
 while not number.isdigit():
-	number = input('Not a positive number. Enter a positive number:  ')
+    number = input('Not a positive number. Enter a positive number:  ')
 while not (int(number) > 0):
-	number = input('Not a positive number. Enter a positive number:  ')
-for i in range(int(number),0, -1):
-	print(i)
-
+    number = input('Not a positive number. Enter a positive number:  ')
+for i in range(int(number), 0, -1):
+    print(i)
 
 
 # 3 - FIZZBUZZ
 
-for i in range(0,101):
-	if i % 3 == 0 and i % 5 == 0:
-		print('FizzBuzz')
-		continue
-	if i % 3 == 0:
-		print('Fizz')
-		continue
-	if i % 5 == 0:
-		print('Buzz')
-		continue
-	print(i)
-
+for i in range(0, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+        continue
+    if i % 3 == 0:
+        print('Fizz')
+        continue
+    if i % 5 == 0:
+        print('Buzz')
+        continue
+    print(i)
 
 
 # 4 - TABLE OF POWERS
 
 go_on = 'y'
 while go_on == 'y':
-	number = input('What number would you like to go up to? ')
-	print()
-	print('Here is your table!')
-	print()
-	print('number \t \t| squared | cubed')
-	print('------ \t \t| ------  | ------')
-	for i in range(1, int(number)+1):
-		print(f'{i} \t \t| {i ** 2} \t \t| {i ** 3}')
-	go_on = input('Would you like to continue? [y/N] ')
-	if go_on == 'y':
-		print()
-		continue
+    number = input('What number would you like to go up to? ')
+    print()
+    print('Here is your table!')
+    print()
+    print('number \t \t| squared | cubed')
+    print('------ \t \t| ------  | ------')
+    for i in range(1, int(number)+1):
+        print(f'{i} \t \t| {i ** 2} \t \t| {i ** 3}')
+    go_on = input('Would you like to continue? [y/N] ')
+    if go_on == 'y':
+        print()
+        continue
 print()
 print('Thank you; good bye!')
-
 
 
 # 5 - NUMBER TO LETTER GRADES
 
 go_on = 'y'
 while go_on == 'y':
-	number_grade = input('Enter a number grade between 0 to 100: ')
-	number_grade = int(number_grade)
-	if number_grade < 60:
-		letter_grade = 'F'
-	elif number_grade >= 60 and number_grade <= 66:
-		letter_grade = 'D'
-	elif number_grade >=67 and number_grade <= 79:
-		letter_grade = 'C'
-	elif number_grade >= 80 and number_grade <= 87:
-		letter_grade = 'B'
-	else:
-		letter_grade = 'A'
-	print('Letter grade is: ' + letter_grade)
-	print()
-	go_on = input('Would you like to continue? [y/N] ')
-	if go_on == 'y':
-		print()
-		continue
+    number_grade = input('Enter a number grade between 0 to 100: ')
+    number_grade = int(number_grade)
+    if number_grade < 60:
+        letter_grade = 'F'
+    elif number_grade >= 60 and number_grade <= 66:
+        letter_grade = 'D'
+    elif number_grade >= 67 and number_grade <= 79:
+        letter_grade = 'C'
+    elif number_grade >= 80 and number_grade <= 87:
+        letter_grade = 'B'
+    else:
+        letter_grade = 'A'
+    print('Letter grade is: ' + letter_grade)
+    print()
+    go_on = input('Would you like to continue? [y/N] ')
+    if go_on == 'y':
+        print()
+        continue
 print()
 print('Thank you; good bye!')
-
 
 
 # 6 - DD'S LIBRARY OF BOOKS
 
 books = [
-	{	'title':'Python Data Analysis',
-		'author':'Fabio Nelli',
-		'genre':'Python' },
-	{	'title':'Adventures in Minecraft',
-		'author':'David Whale',
-		'genre':'Minecraft' },
-	{	'title':'Raspberry Pi By Examples',
-		'author':'Ashwin Pajankar',
-		'genre':'Raspberry Pi' },
-	{	'title':'Python Data Analysis 2',
-		'author':'Fabio Nelli',
-		'genre':'Python' },
-	{	'title':'Adventures in Minecraft 2',
-		'author':'David Whale',
-		'genre':'Minecraft' },
-	{	'title':'Raspberry Pi By Examples 2',
-		'author':'Ashwin Pajankar',
-		'genre':'Raspberry Pi' },
-	{	'title':'Python Data Analysis 3',
-		'author':'Fabio Nelli',
-		'genre':'Python' },
-	{	'title':'Adventures in Minecraft 3',
-		'author':'David Whale',
-		'genre':'Minecraft' },
-	{	'title':'Raspberry Pi By Examples 3',
-		'author':'Ashwin Pajankar',
-		'genre':'Raspberry Pi' }
+    {	'title': 'Python Data Analysis',
+      'author': 'Fabio Nelli',
+      'genre': 'Python'},
+    {	'title': 'Adventures in Minecraft',
+      'author': 'David Whale',
+      'genre': 'Minecraft'},
+    {	'title': 'Raspberry Pi By Examples',
+      'author': 'Ashwin Pajankar',
+      'genre': 'Raspberry Pi'},
+    {	'title': 'Python Data Analysis 2',
+      'author': 'Fabio Nelli',
+      'genre': 'Python'},
+    {	'title': 'Adventures in Minecraft 2',
+      'author': 'David Whale',
+      'genre': 'Minecraft'},
+    {	'title': 'Raspberry Pi By Examples 2',
+      'author': 'Ashwin Pajankar',
+      'genre': 'Raspberry Pi'},
+    {	'title': 'Python Data Analysis 3',
+      'author': 'Fabio Nelli',
+      'genre': 'Python'},
+    {	'title': 'Adventures in Minecraft 3',
+      'author': 'David Whale',
+      'genre': 'Minecraft'},
+    {	'title': 'Raspberry Pi By Examples 3',
+      'author': 'Ashwin Pajankar',
+      'genre': 'Raspberry Pi'}
 ]
 
-for i in range (0,len(books)):
-	print('Title: ' + books[i]['title'])
-	print('Author: ' + books[i]['author'])
-	print('Genre: ' + books[i]['genre'])
-	print()
+for i in range(0, len(books)):
+    print('Title: ' + books[i]['title'])
+    print('Author: ' + books[i]['author'])
+    print('Genre: ' + books[i]['genre'])
+    print()
 
 user_genre = input('Enter a genre: (Python, Minecraft, or Raspberry Pi): ')
 print()
-for i in range (0,len(books)):
-	if books[i]['genre'] == user_genre:
-		print('Title: ' + books[i]['title'])
-		print('Author: ' + books[i]['author'])
-		print('Genre: ' + books[i]['genre'])
-		print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(0, len(books)):
+    if books[i]['genre'] == user_genre:
+        print('Title: ' + books[i]['title'])
+        print('Author: ' + books[i]['author'])
+        print('Genre: ' + books[i]['genre'])
+        print()
--- 4.4_function_exercises.py
+++ 4.4_function_exercises.py
@@ -1,7 +1,7 @@
 def separator():
-	print()
-	print('--------------------------------------------------')
-	print()
+    print()
+    print('--------------------------------------------------')
+    print()
 
 
 separator()
@@ -9,11 +9,14 @@ separator()
 # 1. Define a function named is_two. It should accept one input and
 # return True if the passed input is either the number or the string 2,
 # False otherwise.
+
+
 def is_two(x):
-	if x == '2' or x == 2:
-		return True
-	else:
-		return False
+    if x == '2' or x == 2:
+        return True
+    else:
+        return False
+
 
 print(is_two(2))
 print(is_two('2'))
@@ -24,11 +27,14 @@ separator()
 
 # 2. Define a function named is_vowel. It should return True if the
 # passed string is a vowel, False otherwise.
+
+
 def is_vowel(x):
-	if x.lower() in ['a','e','i','o','u',]:
-		return True
-	else:
-		return False
+    if x.lower() in ['a', 'e', 'i', 'o', 'u', ]:
+        return True
+    else:
+        return False
+
 
 print(is_vowel('a'))
 print(is_vowel('A'))
@@ -40,11 +46,13 @@ separator()
 # passed string is a consonant, False otherwise. Use your is_vowel
 # function to accomplish this.
 
+
 def is_consonant(x):
-	if is_vowel(x):
-		return False
-	else:
-		return True
+    if is_vowel(x):
+        return False
+    else:
+        return True
+
 
 print(is_consonant('b'))
 print(is_consonant('a'))
@@ -52,37 +60,44 @@ print(is_consonant('a'))
 separator()
 
 # 4. Define a function that accepts a string that is a word. The function
-# should capitalize the first letter of the word if the word starts with 
+# should capitalize the first letter of the word if the word starts with
 # consonant.
 
+
 def capitalize(word):
-	if is_consonant(word[0]):
-		return word.capitalize()
-	else:
-		return word
+    if is_consonant(word[0]):
+        return word.capitalize()
+    else:
+        return word
+
 
 print(capitalize('regal'))
 
 separator()
 
 # 5. Define a function named calculate_tip. It should accept a tip
-# percentage (a number between 0 and 1) and the bill total, and return 
+# percentage (a number between 0 and 1) and the bill total, and return
 # the amount to tip.
 
-def calculate_tip(bill,tip):
-	return bill*tip
 
-print(calculate_tip(100,.18))
+def calculate_tip(bill, tip):
+    return bill*tip
+
+
+print(calculate_tip(100, .18))
 
 separator()
 
 # 6. Define a function named apply_discount. It should accept a
 # original price, and a discount percentage, and return the price
 # after the discount is applied.
-def apply_discount(original_price,discount_percentage):
-	return original_price - (original_price * discount_percentage)
 
-print(apply_discount(100,.1))
+
+def apply_discount(original_price, discount_percentage):
+    return original_price - (original_price * discount_percentage)
+
+
+print(apply_discount(100, .1))
 
 separator()
 
@@ -90,12 +105,14 @@ separator()
 # that is a number that contains commas in it as input, and return a
 # number as output.
 
+
 def handle_commas(string):
-	new_string = string
-	for x in string.lower():
-		if x == ',':
-			new_string = new_string.replace(x,'')
-	return new_string
+    new_string = string
+    for x in string.lower():
+        if x == ',':
+            new_string = new_string.replace(x, '')
+    return new_string
+
 
 print(handle_commas(',234,00,0'))
 
@@ -104,17 +121,19 @@ separator()
 # 8. Define a function named get_letter_grade. It should accept a
 # number and return the letter grade associated with that number (A-F).
 
+
 def get_letter_grade(grade):
-	if grade < 60:
-		return 'F'
-	elif grade >= 60 and grade <= 66:
-		return 'D'
-	elif grade >=67 and grade <= 79:
-		return 'C'
-	elif grade >= 80 and grade <= 87:
-		return 'B'
-	else:
-		return 'A'
+    if grade < 60:
+        return 'F'
+    elif grade >= 60 and grade <= 66:
+        return 'D'
+    elif grade >= 67 and grade <= 79:
+        return 'C'
+    elif grade >= 80 and grade <= 87:
+        return 'B'
+    else:
+        return 'A'
+
 
 print(get_letter_grade(81))
 
@@ -123,13 +142,15 @@ separator()
 # 9. Define a function named remove_vowels that accepts a string and
 # returns a string with all the vowels removed.
 
+
 def remove_vowels(word):
-	new_word = word
-	vowels = ('a','e','i','o','u',)
-	for x in word.lower():
-		if x in vowels:
-			new_word = new_word.replace(x,'')
-	return new_word
+    new_word = word
+    vowels = ('a', 'e', 'i', 'o', 'u',)
+    for x in word.lower():
+        if x in vowels:
+            new_word = new_word.replace(x, '')
+    return new_word
+
 
 print(remove_vowels('abracadabra'))
 
@@ -145,16 +166,19 @@ separator()
 # 	- 'First Name' will become 'first_name'
 # 	- '% Completed' will become 'completed'
 
+
 def normalize_name(name):
-	new_name = name
-	for x in name.lower():
-		if x == ' ':
-			new_name = new_name.replace(x,'_')
-	acceptable_characters = ('_','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',)
-	for x in name.lower():
-		if x not in acceptable_characters:
-		 	new_name = new_name.replace(x,'')
-	return (new_name.strip('_')).lower()
+    new_name = name
+    for x in name.lower():
+        if x == ' ':
+            new_name = new_name.replace(x, '_')
+    acceptable_characters = ('_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',)
+    for x in name.lower():
+        if x not in acceptable_characters:
+            new_name = new_name.replace(x, '')
+    return (new_name.strip('_')).lower()
+
 
 print(normalize_name(' % Complete First Name '))
 
@@ -165,16 +189,18 @@ separator()
 # - cumsum([1, 1, 1]) returns [1, 2, 3]
 # - cumsum([1, 2, 3, 4]) returns [1, 3, 6, 10]
 
-def cumsum(list):
-	new_list = []
-	sum = 0
-	for n in list:
-		sum += n
-		new_list.append(sum)
-		#print(sum, end=', ')
-	print(new_list)
 
-list_of_numbers = [1,2,3,4,5,1,1,1,1,1,1,]
+def cumsum(list):
+    new_list = []
+    sum = 0
+    for n in list:
+        sum += n
+        new_list.append(sum)
+        #print(sum, end=', ')
+    print(new_list)
+
+
+list_of_numbers = [1, 2, 3, 4, 5, 1, 1, 1, 1, 1, 1, ]
 cumsum(list_of_numbers)
 
 separator()
