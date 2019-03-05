e063bbfb205211118f496613d3c1f3432630e35a
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-03-04 23:33:46 -0600

    complete function exercises

--- 4.4_function_exercises.py
+++ 4.4_function_exercises.py
@@ -62,6 +62,7 @@ def capitalize(word):
 		return word
 
 print(capitalize('regal'))
+
 separator()
 
 # 5. Define a function named calculate_tip. It should accept a tip
@@ -72,6 +73,7 @@ def calculate_tip(bill,tip):
 	return bill*tip
 
 print(calculate_tip(100,.18))
+
 separator()
 
 # 6. Define a function named apply_discount. It should accept a
@@ -81,6 +83,7 @@ def apply_discount(original_price,discount_percentage):
 	return original_price - (original_price * discount_percentage)
 
 print(apply_discount(100,.1))
+
 separator()
 
 # 7. Define a function named handle_commas. It should accept a string
@@ -95,6 +98,7 @@ def handle_commas(string):
 	return new_string
 
 print(handle_commas(',234,00,0'))
+
 separator()
 
 # 8. Define a function named get_letter_grade. It should accept a
@@ -113,6 +117,7 @@ def get_letter_grade(grade):
 		return 'A'
 
 print(get_letter_grade(81))
+
 separator()
 
 # 9. Define a function named remove_vowels that accepts a string and
@@ -127,9 +132,63 @@ def remove_vowels(word):
 	return new_word
 
 print(remove_vowels('abracadabra'))
+
 separator()
 
-# 10. 
+# 10. Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
+# 	anything that is not a valid python identifier should be removed
+# 	- leading and trailing whitespace should be removed
+# 	- everything should be lowercase
+# 	- spaces should be replaced with underscores
+# 	for example:
+# 	- 'Name' will become 'name'
+# 	- 'First Name' will become 'first_name'
+# 	- '% Completed' will become 'completed'
+
+def normalize_name(name):
+	new_name = name
+	for x in name.lower():
+		if x == ' ':
+			new_name = new_name.replace(x,'_')
+	acceptable_characters = ('_','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',)
+	for x in name.lower():
+		if x not in acceptable_characters:
+		 	new_name = new_name.replace(x,'')
+	return (new_name.strip('_')).lower()
+
+print(normalize_name(' % Complete First Name '))
+
+separator()
+
+# 11. Write a function named cumsum that accepts a list of numbers and
+# returns a list that is the cumulative sum of the numbers in the list.
+# - cumsum([1, 1, 1]) returns [1, 2, 3]
+# - cumsum([1, 2, 3, 4]) returns [1, 3, 6, 10]
+
+def cumsum(list):
+	new_list = []
+	sum = 0
+	for n in list:
+		sum += n
+		new_list.append(sum)
+		#print(sum, end=', ')
+	print(new_list)
+
+list_of_numbers = [1,2,3,4,5,1,1,1,1,1,1,]
+cumsum(list_of_numbers)
+
+separator()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
