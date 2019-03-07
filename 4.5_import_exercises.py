daa4549aae62752013fb6d3350a8dd2bb6da9747
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-03-06 21:25:02 -0600

    add import exercises

--- 4.5_import_exercises.py
+++ 4.5_import_exercises.py
@@ -1,6 +1,6 @@
-from functions_exercises import is_two as twofer
-from functions_exercises import is_two
 import functions_exercises as f
+from functions_exercises import is_two
+from functions_exercises import is_two as twofer
 
 print(f.is_two('three'))
 
